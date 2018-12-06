d425c4c70f7da3d222ec31c3605ca887c02de50f
Mariusz Wachowicz <wachowiczmariusz@gmail.com>
2018-12-06 21:11:44 +0100

    Build rtems-source-builder

---
+++ rsb/setup_and_build.sh
@@ -0,0 +1,18 @@
+RED='\033[0;31m'
+NC='\033[0m'
+
+echo -e "${RED}SOLVING DEPENDENCIES${NS}"
+# sudo apt install bison
+# sudo apt install flex
+# sudo apt install python-dev 
+
+echo -e "${RED}CHECK${NC}"
+rtems-source-builder/source-builder/sb-check
+
+echo -e "${RED}AVAILABLE TARGETS${NC}"
+cd rtems-source-builder/rtems
+../source-builder/sb-set-builder --list-bsets
+
+echo -e "${RED}BULDING${NC}"
+../source-builder/sb-set-builder --prefix=$PWD/4.11 4.11/rtems-sparc
+
